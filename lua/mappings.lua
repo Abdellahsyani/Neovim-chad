96c80a52ea0a622d3da8b639a7547f92bf16b370
Abdellahsyani <syaniabdos@gmail.com>
2025-12-23 20:37:06 +0100

    Initial backup on my NvChad

---
+++ lua/mappings.lua
@@ -0,0 +1,24 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- new tree
+map("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", { desc = "NvimTree Toggle" })
+
+-- Toggle Horizontal Terminal with Space + g + h
+map({ "n", "t" }, "<leader>gh", "<cmd>lua require('nvchad.term').toggle({pos = 'sp', id = 'abc'})<cr>", { desc = "Toggle Horizontal Term" })
+
+-- Copy to system clipboard with Space + y
+map("v", "<leader>y", '"+y', { desc = "Copy to System Clipboard" })
+
+-- open window spliting
+map("n", "<leader>o", "<cmd>vsplit<CR>", { desc = "Vertical Split" })
+
+-- Close current window with Space + c
+map("n", "<leader>c", "<cmd>q<CR>", { desc = "Close window" })
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
