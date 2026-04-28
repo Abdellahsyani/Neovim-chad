b7fc44b890e126d1529749bac0331150694b0f00
Abdellahsyani <syaniabdos@gmail.com>
2026-04-28 19:45:07 +0100

    add copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,17 +20,51 @@ return {
       },
     },
   },
+
+  --copilot
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-l>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        panel = {
+          enabled = false,
+        },
+        filetypes = {
+          yaml = true,
+          markdown = true,
+          help = false,
+          gitcommit = true,
+          c = true,
+          cpp = true,
+          lua = true,
+          python = true,
+        },
+      })
+    end,
+  },
 }
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+-- test new blink
+-- { import = "nvchad.blink.lazyspec" },
 
-  -- {
+-- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    -- 		ensure_installed = {
+      -- 			"vim", "lua", "vimdoc",
+      --      "html", "css"
+      -- 		},
+      -- 	},
+      -- },
